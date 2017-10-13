497cefdbf76f0a472aef6748cf406b12ea46ff1a
Kobayasi, Hiroaki <buribullet@gmail.com>
2017-10-13 18:31:17 +0900

    init

--- lib/File/AddInc.pm
+++ lib/File/AddInc.pm
@@ -5,7 +5,37 @@ use warnings;
 
 our $VERSION = "0.01";
 
+use File::Spec;
+use lib ();
+use Carp ();
 
+use constant DEBUG => $ENV{DEBUG_MOP4IMPORT};
+
+sub import {
+  my ($pack) = @_;
+
+  my ($callpack, $filename, $line) = caller;
+
+  $pack->declare_file_inc($callpack, $filename);
+}
+
+sub declare_file_inc {
+  my ($pack, $callpack, $filename) = @_;
+
+  (my $packfn = $callpack) =~ s,::,/,g;
+  $packfn .= ".pm";
+
+  my $absfn = File::Spec->rel2abs($filename);
+
+  $absfn =~ /\Q$packfn\E\z/
+    or Carp::croak("Can't handle this case! absfn=$absfn; packfn=$packfn");
+
+  my $libdir = substr($absfn, 0, length($absfn) - length($packfn));
+
+  print STDERR "# use lib '$libdir'\n" if DEBUG;
+
+  lib->import($libdir);
+}
 
 1;
 __END__
@@ -14,15 +44,18 @@ __END__
 
 =head1 NAME
 
-File::AddInc - It's new $module
+File::AddInc - find library directory of used file and add it to @INC
 
 =head1 SYNOPSIS
 
-    use File::AddInc;
+    package MyApp::Deep::Module;
+    use File::AddInc; # <-- This will add '../../' to @INC.
+    use MyApp::Util;  # So perl can find MyApp/Util.pm correctly.
 
 =head1 DESCRIPTION
 
-File::AddInc is ...
+File::AddInc finds library toplevel directory for given file and add it to @INC.
+This is useful when you directly used/executed the module.
 
 =head1 LICENSE
 
@@ -36,4 +69,3 @@ it under the same terms as Perl itself.
 Kobayasi, Hiroaki E<lt>buribullet@gmail.comE<gt>
 
 =cut
-
